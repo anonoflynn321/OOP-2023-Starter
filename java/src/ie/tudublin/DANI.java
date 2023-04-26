e701b0b48203f3fb906f71aca6ea2df9c82b6576
anonoflynn321 <123943240+anonoflynn321@users.noreply.github.com>
2023-04-26 17:23:01 +0100

    Update DANI.java
    Trying to make the loadFile() method to no avail

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -1,12 +1,26 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
+	public void loadFile() {
+
+		String[] wordLL;
+		
+		wordLL = loadStrings("small.txt");
+	}
+
+	public String findWord() {
+		return "0"; 
+	}
+
+	public String findFollow() {
+		return "0"; 
+	}
 
 	public void settings() {
 		size(1000, 1000);
