0778e40f9036fe7e122921567dd721a268f7c43d
anonoflynn321 <123943240+anonoflynn321@users.noreply.github.com>
2023-04-26 17:09:51 +0100

    Made a basic version of Word.java
    As above, but i couldn't get toString() working :(

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -7,7 +7,37 @@ public class Word {
     public String word;
     public ArrayList<Follow> list = new ArrayList<Follow>();
 
-    
+    public Word(String word, ArrayList<Follow> list) {
+
+        this.word = word;
+        this.list = list;
+    }
+
+
+    public String getWord() {
+        return word; 
+    }
+
+    public ArrayList<Follow> getList() {
+        return list;
+    }
+
+    /*
+    public String toString() {
+        String bigString; 
+        String tempString;
+
+        for (int i = 0; i < list.size(); i++) {
+            tempString = list.get(i).toString();
+
+            bigString = bigString + " " + tempString;
+        }
+
+        return bigString;
+
+    }
+    */
+
 
 
 
